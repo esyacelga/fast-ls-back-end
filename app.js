330a4e59ff815f669c64a6304a3de6909cadac84
santiago.yacelga <esyacelga@hotmail.com>
2019-08-02 20:24:04 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,48 @@
+//Requires
+var express = require('express');
+
+//inicializar variables
+var app = express();
+
+
+var Connection = require('tedious').Connection;
+var config = {
+    server: '192.168.2.155:1433',  //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'isses', //update me
+            password: '123456'  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'siisspolwebresp'  //update me
+    }
+};
+
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+    // If no error, then good to proceed.
+    console.log("Connected");
+});
+
+
+//Rutas
+app.get('/', (req, res, next)=>{
+    res.status(200).json({
+        ok:true,
+        mensaje:'Peticion realizada correctamente'
+    })
+})
+
+//Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000:\x1b[36m%s\x1b[0m', 'online');
+});
+
+
+
+
+
