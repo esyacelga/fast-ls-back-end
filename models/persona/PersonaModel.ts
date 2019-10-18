5bb3bff298690cb899a1d03da1f51bb323dadc68
santiago.yacelga <esyacelga@hotmail.com>
2019-10-18 16:10:26 -0500

    Correccion consulta

--- models/persona/PersonaModel.ts
+++ models/persona/PersonaModel.ts
@@ -2,6 +2,10 @@ import {model, Schema,} from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 
 const persona = new Schema({
+    avatar: {
+        type: String,
+        default: 'av-1.png'
+    },
     nombres: {type: String, required: [true, 'Los nombres son necesarios']},
     apellidos: {type: String, required: [true, 'Apellidos son necesarios']},
     cedula: {type: String},
