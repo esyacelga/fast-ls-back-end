9f5bdd9fbf7a0c656f7fa2300b5cc5f0c50cc300
santiago.yacelga <esyacelga@hotmail.com>
2020-09-23 22:08:54 -0500

    Correcciones

--- src/models/mensajeria/ArticuloSegmentoModel.ts
+++ src/models/mensajeria/ArticuloSegmentoModel.ts
@@ -4,9 +4,9 @@ import uniqueValidator from "mongoose-unique-validator";
 const articuloSegmento = new Schema({
     descripcion: { type: String, required: [true, 'El descripcion es necesario'] },
     tipoArticulo:{	type: Schema.Types.ObjectId,	ref: 'TipoArticulo' },
+    esServicio:{ type: Boolean },
+    esServicioTransporte:{ type: Boolean },
     estado: { type: Number, required: [true, 'El estado es necesario'] },
-    esServicio:{ type: Boolean, required: [true, 'El estado es necesario'] },
-    esServicioTransporte:{ type: Boolean, required: [true, 'El estado es necesario'] },
 
 });
 articuloSegmento.plugin(uniqueValidator, {message: '{PATH} debe de ser único'});
