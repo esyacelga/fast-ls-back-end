c88b31a5f35977ce3fb7802be46379567e4c481c
santiago.yacelga <esyacelga@hotmail.com>
2020-09-23 21:33:57 -0500

    Correcciones

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ server.app.get('/', (req, res)=>{
 })
 
 // Conectar DB
-mongoose.connect('mongodb://167.172.179.203:27017/volarys',
+mongoose.connect('mongodb://localhost:27017/volarys',
     {useNewUrlParser: true, useCreateIndex: true}, (err) => {
         if (err) throw err;
         console.log('Base de datos ONLINE');
